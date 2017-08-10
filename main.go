3c13be1acb6238a75121717d63c009857e1800a5
Ledka17 <ledkaconf@gmail.com>
2017-08-10 14:51:52 +0300

    end of task

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"bytes"
+)
 
 func main() {
-	fmt.Printf("Hello, world!")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("No such file!")
+		return
+	}
+
+	total := 0
+
+	for i := 0; i < len(args); i++ {
+		file, error := ioutil.ReadFile(args[i])
+
+		if error != nil {
+			return
+		}
+
+		current := bytes.Count(file, []byte{'\n'})
+		total += current
+
+		fmt.Println(current, args[i])
+	}
+
+	if (len(args) > 1) {
+		fmt.Println(total, "total")
+	}
 }
